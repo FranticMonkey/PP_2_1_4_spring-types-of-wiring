8b234006eead1b0d91fbdd790c8154d570998e69
Vladislav Perfiliev <vladis.perfiljev2011@yandex.ru>
2024-02-06 19:57:36 +0400

    Done

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private Deth8 det;
+
+    @Autowired
+    public void setDeth (Deth8 det) {
+        this.det = det;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :(";
     }
 }
